43271861cef33cdf5d7d4c3bf732057cf9b00854
xiao.zeng <543069597@qq.com>
2019-12-11 21:00:46 +0800

    zuul

---
+++ mini-weather-zuul/src/test/java/com/chris/mini/weather/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.mini.weather.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
